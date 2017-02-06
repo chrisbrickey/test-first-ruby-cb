954fde735578c114e0a6a8129356632f268fcfcf
chrisbrickey <cbrickey1@gmail.com>
2017-02-06 14:35:33 -0800

    added padded method; refactored time_string method

--- lib/09_timer.rb
+++ lib/09_timer.rb
@@ -6,36 +6,18 @@ class Timer
     0
   end
 
+  def padded n
+    pad = String.new
+    pad = "0" if n < 9
+    pad + n.to_s
+  end
 
   def time_string
-
-    hours_string = String.new
-    minutes_string = String.new
-    seconds_string = String.new
-
     hours = (@seconds / 3600).to_i
     minutes = ((@seconds % 3600) / 60).to_i
     seconds = @seconds % 60
 
-    if hours < 10
-      hours_string = "0#{hours}:"
-    else
-      hours_string = "#{hours}:"
-    end
-
-    if minutes < 10
-      minutes_string = "0#{minutes}:"
-    else
-      minutes_string = "#{minutes}:"
-    end
-
-    if seconds < 10
-      seconds_string = "0#{seconds}"
-    else
-      seconds_string = "#{seconds}"
-    end
-
-    hours_string + minutes_string + seconds_string
+    padded(hours) + ":" + padded(minutes) + ":" + padded(seconds)
   end
 
 

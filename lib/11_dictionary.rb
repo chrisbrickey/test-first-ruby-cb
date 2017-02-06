be577e12bc3eda331568a3f69f8b67490b1923b3
chrisbrickey <cbrickey1@gmail.com>
2017-02-06 15:21:22 -0800

    added class Dictionary and associated methods

--- lib/11_dictionary.rb
+++ lib/11_dictionary.rb
@@ -1,3 +1,60 @@
 class Dictionary
-  # TODO: your code goes here!
+
+  def initialize
+    @entries = {}
+  end
+
+
+  def entries
+    @entries
+  end
+
+
+  def keywords
+    @entries.keys.sort
+  end
+
+
+  def add x
+    if x.class == String
+      @entries[x] = nil
+    else
+      x.each {|key, value| @entries[key] = value}
+    end
+  end
+
+
+  def include? y
+    if @entries.keys.include?(y)
+      true
+    else
+      false
+    end
+  end
+
+
+  def find z
+    result = {}
+
+    if @entries.include?(z)
+      result = @entries
+    else
+      @entries.each do |key, value|
+        if key.include?(z)
+          result[key] = value
+        end
+      end
+    end
+    result
+  end
+
+
+  def printable
+    new_string = String.new
+    sorted = @entries.keys.sort
+    sorted.each {|key| new_string += "[#{key}] \"#{@entries[key]}\"\n"}
+    new_string.chop
+  end
+
+
 end
